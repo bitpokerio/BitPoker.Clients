88c8f57d90e715b153a836c1c74104af14d7c158
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2017-09-17 15:41:01 +1000

    Add tests and wire up calls

---
+++ src/BitPoker.Clients.Core.xUnitTests/ClientTests.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BitPoker.Clients.Core.xUnitTests
+{
+    public class ClientTests
+    {
+        [Fact]
+        public async Task Should_Get_Balance()
+        {
+            IBalanceClient client = new QBit(true);
+            var actual = await client.GetBalanceAsync("msPJhg9GPzMN6twknwmSQvrUKZbZnk51Tv", 1);
+            
+            Assert.True(actual > 0);
+        }
+    }
+}
